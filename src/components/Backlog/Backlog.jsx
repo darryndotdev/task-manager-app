2cd7ec91ca87d34cc763d149eef188a79b35a17d
Darryn <darryndotdev@gmail.com>
2025-11-09 17:06:13 +0000

    feat: fetch data, local storage functionality and create helper functions

--- src/components/Backlog/Backlog.jsx
+++ src/components/Backlog/Backlog.jsx
@@ -1,5 +1,11 @@
 import styles from './Backlog.module.css';
+import { useOutletContext } from 'react-router-dom';
+
 function Backlog() {
+    const { tasks } = useOutletContext();
+
+    console.log(tasks);
+
     return (
         <div className={styles.backlog}>
             <h1>Backlog</h1>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,61 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar/Sidebar';
 import styles from './AppLayout.module.css';
 
 function AppLayout() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(function () {
+        const stored = localStorage.getItem('tasks');
+        if (stored) {
+            setTasks(JSON.parse(stored));
+        } else {
+            async function getData() {
+                try {
+                    const res = await fetch(`/data/tasks.json`);
+                    const data = await res.json();
+                    setTasks(data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            getData();
+        }
+    }, []);
+
+    function saveTasks(newTasks) {
+        setTasks(newTasks);
+        localStorage.setItem('tasks', JSON.stringify(newTasks));
+    }
+
+    function handleAddTasks(task) {
+        saveTasks([...tasks, task]);
+    }
+
+    function handleUpdateTasks(updatedTask) {
+        saveTasks(
+            tasks.map((task) =>
+                task.id === updatedTask.id ? updatedTask : task
+            )
+        );
+    }
+
+    function handleDeleteTasks(id) {
+        saveTasks(tasks.filter((task) => task.id !== id));
+    }
+
+    const appState = {
+        tasks,
+        handleAddTasks,
+        handleUpdateTasks,
+        handleDeleteTasks,
+    };
+
     return (
         <div className={styles.app}>
             <Sidebar />
-            <Outlet />
+            <Outlet context={appState} />
         </div>
     );
 }
