1c9761ce1ca604c51e4c50be27b1f7ca5b1213e2
Darryn <darryndotdev@gmail.com>
2025-11-12 12:31:58 +0000

    chore: adding box-shadow and changing primary button color to white

--- src/UI/Icons/Add.jsx
+++ src/UI/Icons/Add.jsx
@@ -5,7 +5,7 @@ function Add() {
             height='24px'
             viewBox='0 -960 960 960'
             width='24px'
-            fill='var(--colour)'
+            fill='#fff'
         >
             <path d='M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z' />
         </svg>
