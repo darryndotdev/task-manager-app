d8816acab377620eb70ef0160962576caa3a7a36
Darryn <darryndotdev@gmail.com>
2025-11-12 14:15:37 +0000

    feat: create board item logic

--- src/components/Board/BoardItem.jsx
+++ src/components/Board/BoardItem.jsx
@@ -1,8 +1,34 @@
-function BoardItem() {
+import { Draggable } from '@hello-pangea/dnd';
+import { Link } from 'react-router-dom';
+import Bookmark from '../../UI/Icons/Bookmark';
+import styles from './BoardItem.module.css';
+
+function BoardItem({ task, index }) {
+    console.log(task);
     return (
-        <div>
-            <li>item</li>
-        </div>
+        <Draggable draggableId={task.id} index={index}>
+            {(provided, snapshot) => (
+                <Link to={`/app/ticket/${task.id}`}>
+                    <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className={`${styles.item} ${
+                            snapshot.isDragging ? styles.dragging : ''
+                        }`}
+                    >
+                        <div className={styles.content}>
+                            <h3 className='h4'>{task.title}</h3>
+                            <p>{task.description}</p>
+                        </div>
+                        <div className={styles.ticket}>
+                            <Bookmark />
+                            {task.id}
+                        </div>
+                    </li>
+                </Link>
+            )}
+        </Draggable>
     );
 }
 
